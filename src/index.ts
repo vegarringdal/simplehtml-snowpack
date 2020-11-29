eb22de4d241d3b2fbcab3cce139afaafd8ed5f86
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-11-29 14:55:28 +0100

    chore: index.ts comments

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,15 @@ if (import.meta.env.MODE === "development") {
 
 import("./elements/app-root").then(() => {
   if (document.body && import.meta) {
+
     const tagname = document.body.firstElementChild?.tagName;
     const classnames = document.body.firstElementChild?.className;
     document.body.innerHTML = "";
     document.body.innerHTML = `<${tagname} class="${classnames}"></${tagname}>`; /// little more dynamic
+
+    // this is same as, just easier since chnage to classes in index html will be picked up bu code above
+    //document.body.innerHTML = "";
+    //document.body.innerHTML = `<app-root class="flex flex-col h-full"></app-root>`;
+
   }
 });
